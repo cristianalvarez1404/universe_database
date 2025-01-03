7210b2e85c4a2e2b4d8e0afb55ca1d4856bf4ccb
CristianAlvarez <cristianandres1496@gmail.com>
2025-01-03 13:25:14 -0500

    adding row to the tables star,planet and moon

--- universe.sql
+++ universe.sql
@@ -130,7 +130,7 @@ ALTER SEQUENCE public.moon_moon_id_seq OWNED BY public.moon.moon_id;
 
 CREATE TABLE public.planet (
     planet_id integer NOT NULL,
-    name character varying(50),
+    name character varying(255),
     age_in_millions_of_year integer,
     distance_from_earth integer,
     temperature numeric(4,2),
@@ -250,18 +250,36 @@ INSERT INTO public.galaxy VALUES (6, 'IC 1101', 13, 1004, -270.00, 'Una de las g
 -- Data for Name: moon; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.moon VALUES (1, 'Luna hipotética de Sirio', 100, 9, NULL, 'Sería una luna en el sistema de Sirio B, una enana blanca. Las lunas en este tipo de sistemas pueden ser frías y posiblemente no tener atmósfera.', false, false, 1);
+INSERT INTO public.moon VALUES (2, 'Exoluna en Andrómeda', 1000, 3, NULL, 'Andrómeda I hace referencia a la galaxia de Andrómeda. Las exolunas en esta galaxia podrían ser similares a las lunas de nuestro Sistema Solar, dependiendo de los planetas que las rodeen.', false, false, 2);
+INSERT INTO public.moon VALUES (3, 'Luna en M104 (Galaxia Sombrero)', 1000, 50, NULL, 'El sistema de M104 podría tener planetas con lunas, aunque aún no se ha confirmado una luna específica. La temperatura de la luna dependería del planeta y la estrella.', false, false, 3);
+INSERT INTO public.moon VALUES (4, 'Luna hipotética en M87 (cúmulos globulares)', 1000, 55, NULL, 'Los cúmulos globulares no favorecen la formación de lunas, pero si existieran, serían lunas frías orbitando planetas en sistemas viejos.', false, false, 4);
+INSERT INTO public.moon VALUES (5, 'Luna en el sistema de NGC 2244 (Nebulosa Rosetta)', 4, 4500, NULL, 'NGC 2244 es un cúmulo joven con estrellas calientes. Las lunas en este sistema podrían estar en formación y ser muy jóvenes.', false, false, 5);
+INSERT INTO public.moon VALUES (6, 'Luna en el sistema de IC 1101', 1000, 1, NULL, 'IC 1101 es una galaxia gigante. Si tuviera planetas con lunas, estas lunas podrían estar en sistemas estelares viejos y ser frías o lejanas.', false, false, 6);
 
 
 --
 -- Data for Name: planet; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.planet VALUES (1, 'Sirio B (Planeta potencial alrededor de Sirio)', 100, 9, NULL, 'Sirio B es una enana blanca que podría haber tenido planetas, aunque no se ha confirmado la existencia de uno.', false, false, 1);
+INSERT INTO public.planet VALUES (2, 'Andrómeda I (M31),', 1000, 3, NULL, 'Andrómeda I hace referencia a la galaxia de Andrómeda, donde se han encontrado exoplanetas en varios sistemas.', false, false, 2);
+INSERT INTO public.planet VALUES (3, 'Planeta en el Sistema de HD 100215 (Galaxia Sombrero),', 1000, 50, NULL, 'No se ha confirmado un planeta específico alrededor de HD 100215, pero se ha sugerido la posibilidad de exoplanetas en M104.', false, false, 3);
+INSERT INTO public.planet VALUES (4, 'Planetas en M87 (cúmulos globulares),', 1000, 55, NULL, 'Los planetas en los cúmulos globulares que rodean M87 son hipotéticos, ya que no se han confirmado planetas alrededor de M87 específicamente.', false, false, 4);
+INSERT INTO public.planet VALUES (5, 'Exoplaneta en el Cúmulo NGC 2244 (Nebulosa Rosetta),', 4, 4500, NULL, 'El cúmulo NGC 2244 es joven y contiene estrellas calientes, lo que sugiere que algunos planetas en este sistema podrían ser muy calientes.', false, false, 5);
+INSERT INTO public.planet VALUES (6, 'Planeta en el Sistema de IC 1101', 1000, 100, NULL, 'IC 1101 es una galaxia elíptica gigante, y aunque no se han confirmado planetas específicos, se cree que podrían existir planetas alrededor de estrellas en su periferia.', false, false, 6);
 
 
 --
 -- Data for Name: star; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.star VALUES (1, 'Sirio (α Canis Majoris)', 120, 7, 10.00, 'Es una estrella binaria, con una gigante blanca y una enana blanca. Es la más brillante del cielo nocturno.', false, false, 1);
+INSERT INTO public.star VALUES (2, 'Alpheratz (α Andromedae)', 300, 97, 10.00, 'Estrella gigante azul en la constelación de Andrómeda, parte de la galaxia M31.', false, false, 2);
+INSERT INTO public.star VALUES (3, 'HD 100215 (Galaxia Sombrero)', 400, 50, 5.00, 'Estrella de tipo espectral G5V, perteneciente a la galaxia M104, también conocida como la Galaxia del Sombrero.', false, false, 3);
+INSERT INTO public.star VALUES (4, 'NGC 4486A (Messier 87)', 13, 55, 5.00, 'Estrella ubicada en el cúmulo globular en la galaxia M87, una galaxia elíptica gigante.', false, false, 4);
+INSERT INTO public.star VALUES (5, 'HD 46223 (NGC 2244, Nebulosa Rosetta)', 5, 4500, 35.00, 'Estrella masiva, joven y caliente que forma parte de un cúmulo estelar en la Nebulosa Rosetta.', false, false, 5);
+INSERT INTO public.star VALUES (6, 'IC 1101-1 (Galaxia IC 1101)', 10, 2000, 6.00, 'Estrella ubicada en la galaxia elíptica gigante IC 1101, una de las galaxias más grandes conocidas.', false, false, 6);
 
 
 --
@@ -275,21 +293,21 @@ SELECT pg_catalog.setval('public.galaxy_galaxy_id_seq', 6, true);
 -- Name: moon_moon_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.moon_moon_id_seq', 1, false);
+SELECT pg_catalog.setval('public.moon_moon_id_seq', 6, true);
 
 
 --
 -- Name: planet_planet_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.planet_planet_id_seq', 1, false);
+SELECT pg_catalog.setval('public.planet_planet_id_seq', 6, true);
 
 
 --
 -- Name: star_star_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.star_star_id_seq', 1, false);
+SELECT pg_catalog.setval('public.star_star_id_seq', 6, true);
 
 
 --
